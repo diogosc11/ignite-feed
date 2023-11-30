ecc28c80bf48d29a620203570676eae0e130da91
Diogo Scheffer de Carvalho <38306784+diogosc11@users.noreply.github.com>
2023-11-30 15:30:09 -0300

    add eslint

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,20 +1,22 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
-import styles from './Comment.module.css';
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src='https://github.com/diogosc11.png' alt='' />
+      <img src="https://github.com/diogosc11.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
           <header>
             <div className={styles.authorAndTime}>
               <strong>Diogo Scheffer</strong>
-              <time title='11 de maio às 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+              <time title="11 de maio às 08:13h" dateTime="2022-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button title="Deletar comentário">
               <Trash size={20} />
             </button>
           </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,38 @@
-import { Comment } from './Comment';
-import styles from './Post.module.css';
+import { Comment } from './Comment'
+import styles from './Post.module.css'
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src='https://github.com/diogosc11.png' />
+          <img
+            className={styles.avatar}
+            src="https://github.com/diogosc11.png"
+            alt=""
+          />
           <div className={styles.authorInfo}>
             <strong>Diogo Scheffer</strong>
             <span>Web Developer</span>
           </div>
         </div>
-        <time title='11 de Maio às 08:13h' dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+          Publicado há 1h
+        </time>
       </header>
 
       <div className={styles.content}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quidem minima. Beatae qui esse nemo ab consectetur, cum, reiciendis numquam necessitatibus consequuntur minus dolore ullam! Reiciendis, iusto. Ratione, pariatur ex?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, quidem
+        minima. Beatae qui esse nemo ab consectetur, cum, reiciendis numquam
+        necessitatibus consequuntur minus dolore ullam! Reiciendis, iusto.
+        Ratione, pariatur ex?
       </div>
 
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
-        <textarea placeholder='Deixe um comentário' />
+        <textarea placeholder="Deixe um comentário" />
         <footer>
-          <button type='submit'>Publicar</button>
+          <button type="submit">Publicar</button>
         </footer>
       </form>
 
